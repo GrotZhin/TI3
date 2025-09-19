a79994689d6087d471b87f00da6cf184864320d2
Spewep <arcgabrielsantos@gmail.com>
2025-09-18 22:07:38 -0300

    New Metals info
    Agora tem outros minerios ja e as escritas

--- TI3/Assets/Code/GetColectable.cs
+++ TI3/Assets/Code/GetColectable.cs
@@ -1,16 +1,57 @@
-using System.Collections.Specialized;
 using UnityEngine;
 
 public class GetColectable : MonoBehaviour
 {
-    SerializeField rockindex;
+    [Header("Configurações do Coletável")]
+    [SerializeField] private GameObject inspectItensCanvas;
+    [SerializeField] private GameObject childToActivate;
+    [SerializeField] private GameObject inspectToActivate;
+    [SerializeField] private GameObject[] alwaysActive;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             InspectUiManager manage = FindObjectOfType<InspectUiManager>();
-            manage.OpenMenu();
+            if (manage != null)
+            {
+                manage.OpenMenu();
+            }
+
+            if (inspectItensCanvas != null)
+            {
+                inspectItensCanvas.SetActive(true);
+
+                foreach (Transform child in inspectItensCanvas.transform)
+                {
+                    if (!IsAlwaysActive(child.gameObject))
+                    {
+                        child.gameObject.SetActive(false);
+                    }
+                }
+
+                if (childToActivate != null)
+                {
+                    childToActivate.SetActive(true);
+                }
+
+                if (inspectToActivate != null)
+                {
+                    inspectToActivate.SetActive(true);
+                }
+            }
+
+            Destroy(gameObject);
         }
-        Object.Destroy(gameObject);
+    }
+
+    private bool IsAlwaysActive(GameObject obj)
+    {
+        foreach (var go in alwaysActive)
+        {
+            if (go == obj)
+                return true;
+        }
+        return false;
     }
 }
