6f2d1b62318a7e8f4dbf008dffb5539cc8a6d5aa
GrotZhin <111206067+GrotZhin@users.noreply.github.com>
2025-09-21 02:56:02 -0300

    Alpha0.3.3

--- TI3/Assets/Code/AnimationTest.cs
+++ TI3/Assets/Code/AnimationTest.cs
@@ -4,11 +4,13 @@ public class AnimationTest : MonoBehaviour
 {
     public Animator animator;
     public Animator animator1;
+    public GameObject pedra1;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        pedra1.SetActive(false);
+        
     }
 
     // Update is called once per frame
@@ -17,10 +19,12 @@ public class AnimationTest : MonoBehaviour
         
 
     }
+    
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Push"))
         {
+            pedra1.SetActive(true);
             animator.SetTrigger("plate");
             animator1.SetTrigger("abrir");
             
