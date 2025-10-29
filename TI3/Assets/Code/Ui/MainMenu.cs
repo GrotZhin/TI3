0998f96a5c11671941ada7c4e72576ae0a18c589
Reg-Meme <michaeljacksonnothenoite@gmail.com>
2025-10-29 16:49:45 -0300

    Alpha7.7
    Main Menu now looks a bit better

--- TI3/Assets/Code/Ui/MainMenu.cs
+++ TI3/Assets/Code/Ui/MainMenu.cs
@@ -1,7 +1,7 @@
 using Sfx;
 using UnityEngine;
 using UnityEngine.UI;
-
+using DG.Tweening;
 
 public class MainMenu : MonoBehaviour
 {
@@ -12,10 +12,36 @@ public class MainMenu : MonoBehaviour
     [SerializeField] Slider soundSlider;
     [SerializeField] Slider musicSlider;
     [SerializeField] GameObject optionMenu;
+    public GameObject controls;
+
+    // just copied the pause ani var's
+    public GameObject PMenu;
+    
+    public GameObject OMenu;
+    public CanvasGroup PGroup;
+    public CanvasGroup OGroup;
+    public float BgTopPosY, BgmiddlePosY;
+    public float UpTopPosY, UpmiddlePosY;
+    public float TweenDur;
+    public float BlockerCooldown;
+    float BlockerTimer;
+    [SerializeField] RectTransform Blocker;
+    public static bool DayTime;
+    //pause menu ani
+    [SerializeField] RectTransform Bg;
+    [SerializeField] RectTransform B1;
+    [SerializeField] RectTransform B2;
+    [SerializeField] RectTransform B3;
+    //Options Menu Ani
+    [SerializeField] RectTransform BgOptions;
+     [SerializeField] RectTransform BC;
+    [SerializeField] RectTransform BV;
+    public float BtTopPosx = -555;
     void Start()
     {
         sounds = PlayerPrefs.GetFloat("Sounds", 1.0f);
         music = PlayerPrefs.GetFloat("Music", 1.0f);
+        controls.SetActive(false);
         soundSlider.value = sounds;
         musicSlider.value = music;
     }
@@ -53,5 +79,53 @@ public class MainMenu : MonoBehaviour
         music = musicSlider.value;
         // Update music volume in the game
         AudioListener.volume = music;
+    }
+
+    public void BlockerDo()
+    {
+        Blocker.DOAnchorPosX(410.2825f, 0.1f).SetUpdate(true)
+        .SetAutoKill(false)
+            .OnComplete(async () =>
+            {
+                await Blocker.DOScale(2, 0.8f).SetUpdate(true).AsyncWaitForCompletion();
+                Blocker.DOAnchorPosX(99999, 0.1f).SetUpdate(true);
+            });
+    }
+     public void OptionsMenuani()
+    {
+        BlockerDo();
+        OMenu.SetActive(true);
+        OGroup.DOFade(1, TweenDur).SetEase(Ease.OutFlash).SetUpdate(true);
+        BgOptions.DOAnchorPosX(400, TweenDur).SetEase(Ease.OutFlash).SetUpdate(true);
+
+        BC.DOAnchorPosX(439.995f, TweenDur).SetEase(Ease.OutFlash).SetUpdate(true);
+        BV.DOAnchorPosX(518.995f, TweenDur).SetEase(Ease.OutFlash).SetUpdate(true);
+
+    }
+
+    public async void Back2Menu()
+    {
+        BlockerDo();
+        PMenu.SetActive(true);
+        BgOptions.DOAnchorPosX(BgTopPosY, TweenDur).SetEase(Ease.InFlash).SetUpdate(true);
+        BC.DOAnchorPosX(BtTopPosx, TweenDur).SetEase(Ease.InOutFlash).SetUpdate(true);
+        BV.DOAnchorPosX(BtTopPosx, TweenDur).SetEase(Ease.InOutFlash).SetUpdate(true);
+        await OGroup.DOFade(0, TweenDur).SetEase(Ease.OutFlash).SetUpdate(true).AsyncWaitForCompletion();
+
+
+
+        OMenu.SetActive(false);
+
+
+    }
+    public void Controls()
+    {
+        controls.SetActive(true);
+        Back2Menu();
+    }
+    public void conback()
+    {
+        controls.SetActive(false);
+        OptionsMenuani();
     }
 }
